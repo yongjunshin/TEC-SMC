5f07437e3d1b5cb2defbc3d74929e6f4cfcebd9e
yongjunshin <yjshin@etri.re.kr>
2024-09-12 18:01:45 +0900

    granularity conversion and fix (s, kW, kJ)

--- cases/dummy_app/src/my_util/my_power.py
+++ cases/dummy_app/src/my_util/my_power.py
@@ -1,8 +1,7 @@
 def get_power(meter):
     sample = meter.get_trace()[0]
     duration = sample.duration  # seconds (s)
-    duration = duration * 1000  # milli seconds (ms)
     energy = sum(sample.energy.values())    # micro Joules (uJ)
-    energy = energy * 0.000001  # Joules (J)
+    energy = energy * 0.000000001  # killo Joules (kJ)
     power = energy/duration     # killo Watt (kW)
-    return sample.tag, duration, power, energy   # ms, kW, J
+    return sample.tag, duration, power, energy   # ms, kW, kJ
